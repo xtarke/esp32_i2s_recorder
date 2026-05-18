f96c630923df0ad9f6836368ecebbfab50e2e423
Renan Augusto Starke <xtarke@gmail.com>
2026-05-18 20:37:38 -0300

    Audio is working.

--- main/main.c
+++ main/main.c
@@ -20,7 +20,7 @@
 #include "esp_vfs_fat.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/i2s_pdm.h"
+#include "driver/i2s_std.h"
 #include "driver/gpio.h"
 #include "driver/spi_common.h"
 #include "sdmmc_cmd.h"
@@ -34,7 +34,7 @@
 static const char *TAG = "main";
 
 #define SPI_DMA_CHAN        SPI_DMA_CH_AUTO
-#define NUM_CHANNELS        (1) // For mono recording only!
+#define NUM_CHANNELS        (2) // For mono recording only!
 #define SD_MOUNT_POINT      "/sdcard"
 #define SAMPLE_SIZE         (CONFIG_EXAMPLE_BIT_SAMPLE * 1024)
 #define BYTE_RATE           (CONFIG_EXAMPLE_SAMPLE_RATE * (CONFIG_EXAMPLE_BIT_SAMPLE / 8)) * NUM_CHANNELS
@@ -46,7 +46,8 @@ sdmmc_host_t host = SDSPI_HOST_DEFAULT();
 sdmmc_card_t *card;
 i2s_chan_handle_t rx_handle = NULL;
 
-static int16_t i2s_readraw_buff[SAMPLE_SIZE];
+static int32_t i2s_readraw_buff[SAMPLE_SIZE];
+
 size_t bytes_read;
 const int WAVE_HEADER_SIZE = 44;
 
@@ -57,9 +58,16 @@ void record_wav(uint32_t rec_time)
     int flash_wr_size = 0;
     ESP_LOGI(TAG, "Opening file");
 
-    uint32_t flash_rec_time = BYTE_RATE * rec_time;
+    uint32_t total_audio_bytes = CONFIG_EXAMPLE_SAMPLE_RATE * 4 * 2 * rec_time;
+
+    //uint32_t flash_rec_time = BYTE_RATE * rec_time;
+    
+    uint32_t flash_rec_time = total_audio_bytes;
+    
     const wav_header_t wav_header =
-        WAV_HEADER_PCM_DEFAULT(flash_rec_time, 16, CONFIG_EXAMPLE_SAMPLE_RATE, 1);
+        WAV_HEADER_PCM_DEFAULT(total_audio_bytes, 32, CONFIG_EXAMPLE_SAMPLE_RATE, 2);
+
+    //WAV_HEADER_PCM_DEFAULT (time, sample_bits, sample_rate, channels)
 
     // First check if file exists before creating a new file.
     // struct stat st;
@@ -79,16 +87,20 @@ void record_wav(uint32_t rec_time)
     //fwrite(&wav_header, sizeof(wav_header), 1, f);
     udp_data_send((char *)&wav_header, sizeof(wav_header));
 
-    return;
-
+    int blocks = 0;
+    
     // Start recording
     while (flash_wr_size < flash_rec_time) {
         // Read the RAW samples from the microphone
         if (i2s_channel_read(rx_handle, (char *)i2s_readraw_buff, SAMPLE_SIZE, &bytes_read, 1000) == ESP_OK) {
-            printf("[0] %d [1] %d [2] %d [3]%d ...\n", i2s_readraw_buff[0], i2s_readraw_buff[1], i2s_readraw_buff[2], i2s_readraw_buff[3]);
-            // Write the samples to the WAV file
-            // fwrite(i2s_readraw_buff, bytes_read, 1, f);
+            
+            udp_data_send((char *)&i2s_readraw_buff, bytes_read);
+
             flash_wr_size += bytes_read;
+
+            printf("flash_wr_size %d %d\n", flash_wr_size, bytes_read);
+
+            blocks++;
         } else {
             printf("Read Failed!\n");
         }
@@ -107,31 +119,47 @@ void record_wav(uint32_t rec_time)
 
 void setup_microphone(void)
 {
-#if SOC_I2S_SUPPORTS_PDM2PCM
-    ESP_LOGI(TAG, "Receive PDM microphone data in PCM format");
-#else
-    ESP_LOGI(TAG, "Receive PDM microphone data in raw PDM format");
-#endif  // SOC_I2S_SUPPORTS_PDM2PCM
+
     i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
     ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, NULL, &rx_handle));
 
-    i2s_pdm_rx_config_t pdm_rx_cfg = {
-        .clk_cfg = I2S_PDM_RX_CLK_DEFAULT_CONFIG(CONFIG_EXAMPLE_SAMPLE_RATE),
-        /* The default mono slot is the left slot (whose 'select pin' of the PDM microphone is pulled down) */
-#if SOC_I2S_SUPPORTS_PDM2PCM
-        .slot_cfg = I2S_PDM_RX_SLOT_PCM_FMT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
-#else
-        .slot_cfg = I2S_PDM_RX_SLOT_RAW_FMT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
-#endif
+    i2s_std_config_t std_cfg = {
+        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(CONFIG_EXAMPLE_SAMPLE_RATE),
+        .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
         .gpio_cfg = {
-            .clk = CONFIG_EXAMPLE_I2S_CLK_GPIO,
-            .din = CONFIG_EXAMPLE_I2S_DATA_GPIO,
+            .mclk = I2S_GPIO_UNUSED,    // some codecs may require mclk signal, this example doesn't need it
+            .bclk = CONFIG_EXAMPLE_I2S_CLK_GPIO,
+            .ws   = CONFIG_EXAMPLE_I2S_WS_GPIO,
+            .dout = I2S_GPIO_UNUSED,
+            .din  = CONFIG_EXAMPLE_I2S_DATA_GPIO, // In duplex mode, bind output and input to a same gpio can loopback internally
             .invert_flags = {
-                .clk_inv = false,
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv   = false,
             },
         },
     };
-    ESP_ERROR_CHECK(i2s_channel_init_pdm_rx_mode(rx_handle, &pdm_rx_cfg));
+
+    std_cfg.slot_cfg.slot_bit_width = I2S_SLOT_BIT_WIDTH_32BIT;
+    std_cfg.slot_cfg.slot_mask      = I2S_STD_SLOT_BOTH;
+
+//     i2s_pdm_rx_config_t pdm_rx_cfg = {
+//         .clk_cfg = I2S_PDM_RX_CLK_DEFAULT_CONFIG(CONFIG_EXAMPLE_SAMPLE_RATE),
+//         /* The default mono slot is the left slot (whose 'select pin' of the PDM microphone is pulled down) */
+// #if SOC_I2S_SUPPORTS_PDM2PCM
+//         .slot_cfg = I2S_PDM_RX_SLOT_PCM_FMT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
+// #else
+//         .slot_cfg = I2S_PDM_RX_SLOT_RAW_FMT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
+// #endif
+//         .gpio_cfg = {
+//             .clk = CONFIG_EXAMPLE_I2S_CLK_GPIO,
+//             .din = CONFIG_EXAMPLE_I2S_DATA_GPIO,
+//             .invert_flags = {
+//                 .clk_inv = false,
+//             },
+//         },
+//     };
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_handle, &std_cfg));
     ESP_ERROR_CHECK(i2s_channel_enable(rx_handle));
 }
 
@@ -158,7 +186,7 @@ void app_main(void)
       
     
     // Start Recording
-    record_wav(CONFIG_EXAMPLE_REC_TIME);
+    record_wav(30);
     
     
     
