5f2bbadc4d854544e96eee5a3488733f04fe2a3e
Renan Augusto Starke <xtarke@gmail.com>
2026-05-20 14:41:26 -0300

    Added README

--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# I2S Digital Microphone UDP Recording 
+# I2S Standard Microphone Recording with UDP Streaming Project
+
+In this example, we record a sample audio file captured from two digital microphone son the I2S peripheral. This code initializes the I2S channel in Standard mode (i2s_std.h). The audio is recorded using the WAVE file format. Additionally, this project includes headers for Wi-Fi station (wifi_sta.h) and UDP client (udp_client.h) capabilities, allowing the audio data to be streamed over a network.
+
+## How to use this project
+
+### Hardware Required 
+
+- A development board with ESP32 or ESP32S3 SoC (e.g., ESP32-DevKitC, ESP-WROVER-KIT, etc.).
+- A USB cable for power supply and programming.
+- Two digital I2S microphones (Standard I2S output) 
+
+The digital microphone is connected to the I2S interface, utilizing the i2s_channel_init_std_mode configuration. The digital microphones are connected on the I2S interface `I2S_NUM_0`.
+
+The default GPIO configuration is the following:
+
+|Mic             |  GPIO  |
+|:--------------:|:------:|
+| I2S_CLK_GPIO   | GPIO04 |
+| I2S_DATA_GPIO  | GPIO15 |
+| I2S_WS_GPIO    | GPIO02 |
+
+The microphones conection is the following:
+
+|Mic_1   |  PINS  |
+|:------:|:------:|
+| SD     | I2S_DATA_GPIO |
+| VCC    | 3V3 |
+| GND    | GND |
+| L/R    | GND |
+| WS     | I2S_WS_GPIO |
+| SCK    | I2S_CLK_GPIO|
+
+|Mic_2   |  PINS  |
+|:------:|:------:|
+| SD     | I2S_DATA_GPIO |
+| VCC    | 3V3 |
+| GND    | GND |
+| L/R    | 3V3 |
+| WS     | I2S_WS_GPIO |
+| SCK    | I2S_CLK_GPIO|
+
+**Make sure one mic has 3V3 in the L/R signal and the other has GND.**
+
+
+## Configure the Project
+
+In the Configuration menu (idf.py menuconfig):
+
+- Use I2S Configuration to assign the I2S peripheral GPIOs, Sample Rate, and Bit Sample sizes. **Tested only with defulat configurations**.
+
+- Use WiFi Configuration to configure your Wi-Fi credentials.
+
+- Use UDP Server Configuration to change UDP target IP/Port for the network streaming features included in the project.
+
+```bash
+idf.py build flash monitor -p SERIAL_PORT
+```
+
+(To exit the serial monitor, type Ctrl-].) See the Getting Started Guide for full steps to configure and use ESP-IDF to build projects.
+
+* [ESP-IDF Getting Started Guide on ESP32](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/index.html)
+
+### Project output 
+
+Running this project, you will see follwing outputs logs:
+
+#### ESP32 monitor
+
+```
+(...)
+I (12938) esp_netif_handlers: sta ip: 192.168.0.136, mask: 255.255.255.0, gw: 192.168.0.1
+I (12938) wifi station: got ip:192.168.0.136
+I (12938) wifi station: connected to ap SSID:------- password: --------
+I (12948) example: Socket created, sending to 192.168.0.132:3333
+I (12948) main: Starting recording for 30 seconds!
+I (12948) main: Opening file
+udp_wr_size: 32768 btes read: 32768
+udp_wr_size: 65536 btes read: 32768
+udp_wr_size: 98304 btes read: 32768
+udp_wr_size: 131072 btes read: 32768
+udp_wr_size: 163840 btes read: 32768
+udp_wr_size: 196608 btes read: 32768
+udp_wr_size: 229376 btes read: 32768
+udp_wr_size: 262144 btes read: 32768
+udp_wr_size: 294912 btes read: 32768
+udp_wr_size: 327680 btes read: 32768
+(...)
+udp_wr_size: 3670016 btes read: 32768
+udp_wr_size: 3702784 btes read: 32768
+udp_wr_size: 3735552 btes read: 32768
+udp_wr_size: 3768320 btes read: 32768
+udp_wr_size: 3801088 btes read: 32768
+udp_wr_size: 3833856 btes read: 32768
+udp_wr_size: 3866624 btes read: 32768
+I (43168) main: Recording done!
+I (43168) main: File send!. Make sure UDP server is restarted.
+I (43168) main_task: Returned from app_main()
+```
+
+#### Python UDP server (udp_server_bin.py)
+
+```
+UDP server listening on 0.0.0.0:3333...
+Ready to record data into 'received_file.wav'...
+Received 44 bytes from ('192.168.0.136', 58957) (0)
+Received 32768 bytes from ('192.168.0.136', 58957) (1)
+Received 32768 bytes from ('192.168.0.136', 58957) (2)
+Received 32768 bytes from ('192.168.0.136', 58957) (3)
+Received 32768 bytes from ('192.168.0.136', 58957) (4)
+Received 32768 bytes from ('192.168.0.136', 58957) (5)
+Received 32768 bytes from ('192.168.0.136', 58957) (6)
+Received 32768 bytes from ('192.168.0.136', 58957) (7)
+(...)
+Received 32768 bytes from ('192.168.0.136', 58957) (112)
+Received 32768 bytes from ('192.168.0.136', 58957) (113)
+Received 32768 bytes from ('192.168.0.136', 58957) (114)
+Received 32768 bytes from ('192.168.0.136', 58957) (115)
+Received 32768 bytes from ('192.168.0.136', 58957) (116)
+Received 32768 bytes from ('192.168.0.136', 58957) (117)
+Received 32768 bytes from ('192.168.0.136', 58957) (118)
+```
+
+After the reception, a file **received_file.wav** is created. Don't forter to stop the server after the file is received. (To exit the udp server, type Ctrl-C.)
+
+## Troubleshooting
+
+* Program upload failure
+
+    * Hardware connection is not correct: run `idf.py -p PORT monitor`, and reboot your board to see if there are any output logs.
+    * The baud rate for downloading is too high: lower your baud rate in the `menuconfig` menu, and try again.
+
+For any technical queries, please open an [issue](https://github.com/espressif/esp-idf/issues) on GitHub.
--- main/main.c
+++ main/main.c
@@ -33,17 +33,9 @@
 
 static const char *TAG = "main";
 
-#define SPI_DMA_CHAN        SPI_DMA_CH_AUTO
-#define NUM_CHANNELS        (2) // For mono recording only!
-#define SD_MOUNT_POINT      "/sdcard"
 #define SAMPLE_SIZE         (CONFIG_EXAMPLE_BIT_SAMPLE * 1024)
 #define BYTE_RATE           (CONFIG_EXAMPLE_SAMPLE_RATE * (CONFIG_EXAMPLE_BIT_SAMPLE / 8)) * NUM_CHANNELS
 
-// When testing SD and SPI modes, keep in mind that once the card has been
-// initialized in SPI mode, it can not be reinitialized in SD mode without
-// toggling power to the card.
-sdmmc_host_t host = SDSPI_HOST_DEFAULT();
-sdmmc_card_t *card;
 i2s_chan_handle_t rx_handle = NULL;
 
 static int32_t i2s_readraw_buff[SAMPLE_SIZE];
@@ -55,50 +47,30 @@ const int WAVE_HEADER_SIZE = 44;
 void record_wav(uint32_t rec_time)
 {
     // Use POSIX and C standard library functions to work with files.
-    int flash_wr_size = 0;
+    int udp_wr_size = 0;
     ESP_LOGI(TAG, "Opening file");
 
+    /* SAMPLE_RATE * 4 bytes (32 or 24 bits) * channels * rec_time */
     uint32_t total_audio_bytes = CONFIG_EXAMPLE_SAMPLE_RATE * 4 * 2 * rec_time;
 
-    //uint32_t flash_rec_time = BYTE_RATE * rec_time;
-    
-    uint32_t flash_rec_time = total_audio_bytes;
-    
     const wav_header_t wav_header =
         WAV_HEADER_PCM_DEFAULT(total_audio_bytes, 32, CONFIG_EXAMPLE_SAMPLE_RATE, 2);
 
-    //WAV_HEADER_PCM_DEFAULT (time, sample_bits, sample_rate, channels)
-
-    // First check if file exists before creating a new file.
-    // struct stat st;
-    // if (stat(SD_MOUNT_POINT"/record.wav", &st) == 0) {
-    //     // Delete it if it exists
-    //     unlink(SD_MOUNT_POINT"/record.wav");
-    // }
-
-    // // Create new WAV file
-    // FILE *f = fopen(SD_MOUNT_POINT"/record.wav", "a");
-    // if (f == NULL) {
-    //     ESP_LOGE(TAG, "Failed to open file for writing");
-    //     return;
-    // }
-
-    // Write the header to the WAV file
-    //fwrite(&wav_header, sizeof(wav_header), 1, f);
+    /* Send WAVE header */
     udp_data_send((char *)&wav_header, sizeof(wav_header));
 
     int blocks = 0;
     
     // Start recording
-    while (flash_wr_size < flash_rec_time) {
+    while (udp_wr_size < total_audio_bytes) {
         // Read the RAW samples from the microphone
         if (i2s_channel_read(rx_handle, (char *)i2s_readraw_buff, SAMPLE_SIZE, &bytes_read, 1000) == ESP_OK) {
             
             udp_data_send((char *)&i2s_readraw_buff, bytes_read);
 
-            flash_wr_size += bytes_read;
+            udp_wr_size += bytes_read;
 
-            printf("flash_wr_size %d %d\n", flash_wr_size, bytes_read);
+            printf("udp_wr_size: %d btes read: %d\n", udp_wr_size, bytes_read);
 
             blocks++;
         } else {
@@ -108,13 +80,7 @@ void record_wav(uint32_t rec_time)
 
     ESP_LOGI(TAG, "Recording done!");
     // fclose(f);
-    ESP_LOGI(TAG, "File written on SDCard");
-
-    // All done, unmount partition and disable SPI peripheral
-    // esp_vfs_fat_sdcard_unmount(SD_MOUNT_POINT, card);
-    // ESP_LOGI(TAG, "Card unmounted");
-    // Deinitialize the bus after all devices are removed
-    // spi_bus_free(host.slot);
+    ESP_LOGI(TAG, "File send!. Make sure UDP server is restarted.");
 }
 
 void setup_microphone(void)
@@ -143,54 +109,27 @@ void setup_microphone(void)
     std_cfg.slot_cfg.slot_bit_width = I2S_SLOT_BIT_WIDTH_32BIT;
     std_cfg.slot_cfg.slot_mask      = I2S_STD_SLOT_BOTH;
 
-//     i2s_pdm_rx_config_t pdm_rx_cfg = {
-//         .clk_cfg = I2S_PDM_RX_CLK_DEFAULT_CONFIG(CONFIG_EXAMPLE_SAMPLE_RATE),
-//         /* The default mono slot is the left slot (whose 'select pin' of the PDM microphone is pulled down) */
-// #if SOC_I2S_SUPPORTS_PDM2PCM
-//         .slot_cfg = I2S_PDM_RX_SLOT_PCM_FMT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
-// #else
-//         .slot_cfg = I2S_PDM_RX_SLOT_RAW_FMT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
-// #endif
-//         .gpio_cfg = {
-//             .clk = CONFIG_EXAMPLE_I2S_CLK_GPIO,
-//             .din = CONFIG_EXAMPLE_I2S_DATA_GPIO,
-//             .invert_flags = {
-//                 .clk_inv = false,
-//             },
-//         },
-//     };
     ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_handle, &std_cfg));
     ESP_ERROR_CHECK(i2s_channel_enable(rx_handle));
 }
 
 void app_main(void)
 {
-   
     ESP_LOGI(TAG, "UDP mic recorder");
     
-    
-    // Mount the SDCard for recording the audio file
-    // mount_sdcard();
-    // Acquire a I2S PDM channel for the PDM digital microphone
-        
     /* Setup and connect WiFi STA mode with static credentials */
     setup_wifi_static_cred();
 
     /* Setup UDP client */
     setup_udp_client();
     
-    
+    /* Setup I2S interface  */
     setup_microphone();
-    // ESP_LOGI(TAG, "Starting recording for %d seconds!", CONFIG_EXAMPLE_REC_TIME);
-    
-      
-    
-    // Start Recording
-    record_wav(30);
-    
-    
-    
-    // // Stop I2S driver and destroy
-    // ESP_ERROR_CHECK(i2s_channel_disable(rx_handle));
-    // ESP_ERROR_CHECK(i2s_del_channel(rx_handle));
+
+    ESP_LOGI(TAG, "Starting recording for %d seconds!", CONFIG_EXAMPLE_REC_TIME);
+    record_wav(CONFIG_EXAMPLE_REC_TIME);
+
+    // Stop I2S driver and destroy
+    ESP_ERROR_CHECK(i2s_channel_disable(rx_handle));
+    ESP_ERROR_CHECK(i2s_del_channel(rx_handle));
 }
